16af598f1bd29e321ca75b5973f1263b3efb56ad
73981 <73981@DESKTOP-17TNRGB>
2022-07-26 05:41:39 +0530

    first commit
    upload all the files

---
+++ learning.phase1/src/com/learning/phase1/classes/Caculator.java
@@ -0,0 +1,42 @@
+package com.learning.phase1.classes;
+
+import java.util.Scanner;
+
+public class Caculator {
+	
+	public static void main(String[] args) {
+		int n1,n2;
+		char choice;
+		double operation=0.0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 1st number: ");
+		n1 = sc.nextInt();
+		
+		System.out.println("Enter 2nd number: ");
+		n2 = sc.nextInt();
+		
+		System.out.println("choice");
+		choice = sc.next().charAt(0);
+		
+		switch(choice) {
+		case 'a' : operation = n1 + n2;
+				   break;
+			
+		case 'b' : operation = n1 - n2;
+		           break;
+		           
+		case 'c' : operation = n1*n2;
+		           break;
+		           
+		case 'd' : operation = n1/n2;
+		           break;
+		           
+		}
+	
+		System.out.println(n1+" "+choice+" "+n2+" "+operation);
+	}
+	
+	
+	
+
+}
