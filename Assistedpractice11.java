b19f220966974beef9bd2e7226ff0e0fcdb38d75
shreya758 <7398187805a@gmail.com>
2022-08-01 04:38:41 +0530

    Add files via upload

---
+++ Assistedpractice11.java
@@ -0,0 +1,15 @@
+package threadcollection;
+
+public class Assistedpractice11 {
+	public void run()
+ 	{
+  		System.out.println("concurrent thread started running..");
+}
+ 	public static void main( String args[] )
+ 	{
+  		MyThread mt = new  MyThread();
+  		mt.start();
+ 	}
+}
+
+
---
+++ MyThread.java
@@ -0,0 +1,10 @@
+package threadcollection;
+
+public class MyThread {
+
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
